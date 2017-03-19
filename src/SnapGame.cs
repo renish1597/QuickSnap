00cfbb58856044bc1e5ed228d480f3c29446179a
lyhuy55 <lyhuy55@gmail.com>
2017-03-19 15:35:34 +1100

    change

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -53,10 +53,11 @@ namespace CardGames
 			if (top != null)
 			{
                 SwinGame.LoadFontNamed("GameFont", "Chunkfive.otf", 24);
-                SwinGame.DrawText("" + myGame.Score(0), Color.White, "GameFont", 0, 30);
+                SwinGame.DrawText("" + myGame.Score(0), Color.White, "GameFont", 0, 20);
+                SwinGame.DrawText("" + myGame.Score(1), Color.White, "GameFont", 0, 30);
                 SwinGame.DrawText ("Top Card is " + top.ToString (), Color.RoyalBlue, 0, 20);
-				SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
-				SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
+                //SwinGame.DrawText ("Player 1 score: " + myGame.Score(0), Color.RoyalBlue, 0, 30);
+                //SwinGame.DrawText ("Player 2 score: " + myGame.Score(1), Color.RoyalBlue, 0, 40);
 				SwinGame.DrawCell (SwinGame.BitmapNamed ("Cards"), top.CardIndex, 521, 153);
                
 			}
